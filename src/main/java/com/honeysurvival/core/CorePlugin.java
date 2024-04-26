976a21d5bdc77d08efea4bb1271d7c00192f7635
Magnus T.K <74688700+codebyxemu@users.noreply.github.com>
2024-04-26 23:26:40 +0200

    CommandAPI Fix

--- src/main/java/com/honeysurvival/core/CorePlugin.java
+++ src/main/java/com/honeysurvival/core/CorePlugin.java
@@ -20,30 +20,35 @@ import java.util.List;
 @Getter
 public final class CorePlugin extends JavaPlugin {
 
-    private final ComponentManager componentManager = ComponentManager.register(this, List.of(
-            MainConfig.createDefault(this),
-            MessagesConfig.createDefault(this),
-            ReportDataConfig.createDefault(this),
-            new IntegrationComponent(this),
-            new ReportLoader(this),
-            new ReportComponent(this)
-    ));
+    private ComponentManager componentManager;
 
     @Override
     public void onLoad() {
         CommandAPI.onLoad(new CommandAPIBukkitConfig(this).silentLogs(true));
+
+        this.componentManager = ComponentManager.register(this, List.of(
+                MainConfig.createDefault(this),
+                MessagesConfig.createDefault(this),
+                ReportDataConfig.createDefault(this),
+                new IntegrationComponent(this),
+                new ReportLoader(this),
+                new ReportComponent(this)
+        ));
+
         componentManager.loadComponents();
     }
 
     @Override
     public void onEnable() {
         CommandAPI.onEnable();
+
         componentManager.enableComponents();
     }
 
     @Override
     public void onDisable() {
         CommandAPI.onDisable();
+
         componentManager.disableComponents();
     }
 
